9aa5c6415170ef0ef8f7daaf041df206d56854ba
junichi.baba <junichi.baba@stylez.co.jp>
2025-06-16 11:15:44 +0900

    トラックボールスクロール方向を調整するキーをカスタム定義

--- qmk_firmware/keyboards/tsumkey_split60/keymaps/vial/keymap.c
+++ qmk_firmware/keyboards/tsumkey_split60/keymaps/vial/keymap.c
@@ -1,2 +1,25 @@
 #include QMK_KEYBOARD_H
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {};
+
+enum custom_keycodes {
+    ROT_R15 = SAFE_RANGE,
+    ROT_L15
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case ROT_R15:
+            if (record->event.pressed) {
+                angle += 15;
+                // 必要に応じて最大値・最小値の制御
+            }
+            return false;
+        case ROT_L15:
+            if (record->event.pressed) {
+                angle -= 15;
+                // 必要に応じて最大値・最小値の制御
+            }
+            return false;
+    }
+    return true;
+}
